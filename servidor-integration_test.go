ad09cad895668d53bb5a18e58b72388b5b68cead
Mariana Victor <marianavictorf@gmail.com>
2024-01-05 10:29:47 -0300

    reorganize files again

--- servidor-integration_test.go
+++ servidor-integration_test.go
@@ -22,19 +22,5 @@ func TestRegistrarVitoriasEBuscarEstasVitorias(t *testing.T) {
 
 		verificarCorpoRequisicao(t, resposta.Body.String(), "3")
 	})
-	t.Run("Em base de dados", func(t *testing.T) {
-		jogador := "Maria"
-		armazenamento := CriaArmazenamentoJogadorEmDB(zeraTabelaNaDB("tabelateste2"))
-		servidor := &ServidorJogador{armazenamento}
 
-		servidor.ServeHTTP(httptest.NewRecorder(), novaRequisicaoRegistrarVitoriaPost(jogador))
-		servidor.ServeHTTP(httptest.NewRecorder(), novaRequisicaoRegistrarVitoriaPost(jogador))
-		servidor.ServeHTTP(httptest.NewRecorder(), novaRequisicaoRegistrarVitoriaPost(jogador))
-
-		resposta := httptest.NewRecorder()
-		servidor.ServeHTTP(resposta, novaRequisicaoObterPontuacao(jogador))
-		verificarRespostaCodigoStatus(t, resposta.Code, http.StatusOK)
-
-		verificarCorpoRequisicao(t, resposta.Body.String(), "3")
-	})
 }
