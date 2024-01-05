38a7bfe0df6842320260d1b051cc028e9de4c33c
Mariana Victor <marianavictorf@gmail.com>
2024-01-05 10:50:33 -0300

    reorganizando em dois repos

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module github.com/marianavif/go-api
 
 go 1.17
 
-require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -12,11 +12,4 @@ func main() {
 	if err := http.ListenAndServe(":5000", servidor); err != nil {
 		log.Fatalf("não foi possível escutar na porta 5000 %v", err)
 	}
-
-	// main para servidor com base de dados postgres
-	/* 	servidorDB := &ServidorJogador{CriaArmazenamentoJogadorEmDB("<nome-da-tabela-criada>")} // alterar para nome da tabela usada
-
-	if err := http.ListenAndServe(":5000", servidorDB); err != nil {
-		log.Fatalf("não foi possível escutar na porta 5000 %v", err)
-	} */
 }
